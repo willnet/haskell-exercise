d3c353268e1b0d438ea00385215e5124ae43b4d2
willnet <netwillnet@gmail.com>
2012-07-05 23:52:02 +0900

    Add function in section 4 (in progress)

---
+++ recursive.hs
@@ -0,0 +1,11 @@
+replicate' :: Int -> a -> [a]
+replicate' n x
+  | n <= 0 = []
+  | otherwise = x : replicate' (n-1) x
+
+take' :: Int -> [a] -> [a]
+take' n _
+  | n <= 0 = []
+take' _ [] = []
+take' n (x:xs) = x : take' (n-1) xs
+               
