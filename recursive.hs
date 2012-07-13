4d5c9c09d0b8d6446b22a47d9b340cddf6ea4ee4
willnet <netwillnet@gmail.com>
2012-07-13 20:59:57 +0900

    Add function in section 4

--- recursive.hs
+++ recursive.hs
@@ -8,4 +8,28 @@ take' n _
   | n <= 0 = []
 take' _ [] = []
 take' n (x:xs) = x : take' (n-1) xs
-               
+
+reverse' :: [a] -> [a]
+reverse' [] = []
+reverse' (x:xs) = reverse' xs ++ [x]
+
+repeat' :: a -> [a]
+repeat' x = x: repeat' x
+
+zip' :: [a] -> [b] -> [(a,b)]
+zip' [] _ = []
+zip' _ [] = []
+zip' (x:xs) (y:ys) = (x,y) : zip' xs ys
+
+elem' :: (Eq a) => a -> [a] -> Bool
+elem' a [] = False
+elem' a (x:xs)
+  | a == x = True
+  | otherwise = a `elem'` xs
+
+quicksort :: (Ord a) => [a] -> [a]
+quicksort [] = []
+quicksort (x:xs) =
+  let smallerOrEqual = [a | a <- xs, a <= x]
+      larger = [a| a <- xs, a > x]
+  in quicksort smallerOrEqual ++ [x] ++ quicksort larger
